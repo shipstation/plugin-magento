ac428b1112c55eaecf6fdaf61480c25a5e91ac29
Martin J <j0um@hotmail.com>
2020-12-16 17:15:26 -0500

    Fixes order exportation : All stores data will be exported instead of default store only. (#8)
    Co-authored-by: mjourneault <mjourneault@gmail.com>

--- Api/Controller/Auctane/Index.php
+++ Api/Controller/Auctane/Index.php
@@ -11,15 +11,17 @@ use Magento\Backend\Model\Auth\Credential\StorageInterface;
 use Magento\Backend\Model\View\Result\RedirectFactory;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Exception\AuthorizationException;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Store\Model\StoreManagerInterface;
-use Magento\Framework\Exception\AuthorizationException;
 use Zend\Http\Response;
 use Zend\Json\Server\Response\Http;
+use Magento\Store\Model\Store;
+
 
 class Index extends Action implements CsrfAwareActionInterface
 {
@@ -105,40 +107,37 @@ class Index extends Action implements CsrfAwareActionInterface
      */
     public function execute()
     {
-        if(!$this->authenticator->authenticate())
-        {
+        if (!$this->authenticator->authenticate()) {
             $this->getResponse()->setHeader('WWW-Authenticate: ', 'Basic realm=ShipStation', true);
             $this->getResponse()->setHeader('Content-Type', 'text/xml; charset=UTF-8', true);
             $result = $this->dataHelper->fault(401, 'Authentication failed');
             $this->getResponse()->setBody($result);
             return false;
         }
-        else
-        {
-            /** @var $request \Magento\Framework\App\Request\Http */
-            $request = $this->getRequest();
-            //Get the requested action
-            $action = $request->getParam('action');
-            try {
-                switch ($action) {
-                    case 'export':
-                        $storeId = $this->storeManager->getStore()->getId();
-                        $result = $this->export->process($request, $this->getResponse(), $storeId);
-                        break;
 
-                    case 'shipnotify':
-                        $result = $this->shipNotify->process();
-                        // if there hasn't been an error then "200 OK" is given
-                        break;
-                }
-            } catch (LocalizedException $e) {
-                $this->_response->setStatusCode(Response::STATUS_CODE_400);
-                $result = $this->dataHelper->fault(Response::STATUS_CODE_400, $e->getMessage());
-            } catch (Exception $fault) {
-                $result = $this->dataHelper->fault($fault->getCode(), $fault->getMessage());
+        /** @var $request \Magento\Framework\App\Request\Http */
+        $request = $this->getRequest();
+        //Get the requested action
+        $action = $request->getParam('action');
+
+        try {
+            switch ($action) {
+                case 'export':
+                    $result = $this->export->process($request, $this->getResponse(), Store::DEFAULT_STORE_ID);
+                    break;
+
+                case 'shipnotify':
+                    $result = $this->shipNotify->process();
+                    // if there hasn't been an error then "200 OK" is given
+                    break;
             }
-
-            $this->getResponse()->setBody($result);
+        } catch (LocalizedException $e) {
+            $this->_response->setStatusCode(Response::STATUS_CODE_400);
+            $result = $this->dataHelper->fault(Response::STATUS_CODE_400, $e->getMessage());
+        } catch (Exception $fault) {
+            $result = $this->dataHelper->fault($fault->getCode(), $fault->getMessage());
         }
+
+        $this->getResponse()->setBody($result);
     }
 }
